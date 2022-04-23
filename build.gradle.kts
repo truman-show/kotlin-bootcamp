b4c4e2e787a592cbe0a841d3f5fb3cd97cede352
ljihoon <ljh86091@gmail.com>
2022-04-24 01:43:45 +0900

    kotest 테스트 프레임워크를 추가하라
    참고 : https://techblog.woowahan.com/5825/

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,12 +7,17 @@ plugins {
 group = "com.bootcamp"
 version = "1.0-SNAPSHOT"
 
+object Versions {
+    const val KOTEST = "5.2.2"
+}
+
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    testImplementation(kotlin("test"))
+    testImplementation("io.kotest:kotest-runner-junit5:5.2.2")
+    testImplementation("io.kotest:kotest-assertions-core:5.2.2")
 }
 
 tasks.test {
